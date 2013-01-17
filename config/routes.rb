89e9c2a0dc86d3d2502772c6972d869b5b2fc98f
Jean-Michel Bakker <jeanmichel.bakker@gmail.com>
2013-01-17 14:56:47 +0100

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 FirstApp::Application.routes.draw do
+  resources :microposts
+
+
+  resources :receipts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
